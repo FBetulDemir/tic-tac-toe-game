094ad671952c61640784af5ae8c296eea61e793c
FBetulDemir <fatmabetuld@gmail.com>
2024-06-02 12:38:43 +0200

    Added a function to check the winning combos

--- app.js
+++ app.js
@@ -6,30 +6,57 @@ const squares = Array.from(document.querySelectorAll('#board div'));
 const messages = document.querySelector('.message');
 let board;
 let turn = 'X';
+let win;
 
 document.getElementById('board').addEventListener('click', handleTurn);
+
 
 function handleTurn(event){
     let idx = squares.findIndex(function(square){
         return square === event.target;
     });
     board[idx] = turn;
-    if(turn === 'X'){
-        turn='O'
-    }else{
-        turn='X'
-    };
+    win = getWinner();
+    // if(turn === 'X'){
+    //     turn='O'
+    // }else{
+    //     turn='X'
+    // };
+    win = getWinner();
     render();
 };
-console.log(turn);
 
+
+
+function getWinner(){
+    let winner = null;
+    if(board[0]===board[1]&& board[1]===board[2]){
+
+    }else if(board[3]===board[4]&& board[4]===board[5]){
+
+    }else if(board[6]===board[7]&& board[7]===board[8]){
+
+    }else if(board[0]===board[3]&& board[3]===board[6]){
+
+    }else if(board[1]===board[4]&& board[4]===board[7]){
+
+    }else if(board[2]===board[5]&& board[5]===board[8]){
+
+    }else if(board[0]===board[4]&& board[4]===board[8]){
+
+    }else if(board[2]===board[4]&& board[4]===board[6]){
+
+    };
+    return winner;
+};
 
 function render (){
     board.forEach(function(cell, idx){
         squares[idx].textContent = cell;
     });
-    messages.textContent = ``
+    messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
 };
+
 
 function init (){
     board = [
@@ -40,6 +67,8 @@ function init (){
 
     render();
 };
+
+
 
 
 
